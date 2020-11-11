87136ddff67eba91c1a63ceeb2f1c659175f3ab3
Rupa <rupa211984@gmail.com>
2020-11-10 16:33:31 -0800

    Readme

---
+++ javascript/roman_numeral.js
@@ -0,0 +1,19 @@
+function romanNumeral(string) {
+  // type your code here
+}
+
+if (require.main === module) {
+  // add your own tests in here
+  console.log("Expecting: 3");
+  console.log(romanNumeral([2, 1, 3, 3, 2]));
+
+  console.log("");
+
+  console.log("Expecting: -1");
+  console.log(romanNumeral([1, 2, 3, 4]));
+}
+
+module.exports = romanNumeral;
+
+// Please add your pseudocode to this file
+// And a written explanation of your solution
